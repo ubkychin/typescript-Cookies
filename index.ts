2d7b4985d90f4a04e2b109e23f1766019726f3de
ubkychin <anguyen@swin.edu.au>
2021-07-19 11:56:08 +1000

    Task ready

--- index.ts
+++ index.ts
@@ -1,2 +1,46 @@
 // Import stylesheets
 import './style.css';
+import { Cookie } from './models/Cookie';
+
+// Create a array/list of cookies named cookies
+
+
+// create an init() function
+// init() will create two cookies called Cookie1 and Cookie2, and add them to the array of cookies
+function init() {
+  //TODO: add code here
+  // create the two cookies
+  // add them to the array
+  // add them as options in the select/dropdown (cookieSelector) element
+
+
+  // initialise the cookieColour-inp to the colour of the first cookie created
+
+  updateDisplay();
+}
+
+//TODO: this function needs to go through the list of cookies and draw them to cookiesDiv
+// create the cookies as divs with the class name of cookie - see style.css
+// number of chocolatechips needs to be shown on the cookie
+function drawCookies() {
+
+}
+
+//TODO: this fuction needs to be triggered by button changeColour-btn
+// upon pressing the button it should change the colour of the cookie selected in the dropdown to the colour typed in the input element (cookieColour-inp)
+function changeColour() {
+
+  updateDisplay();
+}
+
+//TODO: this fuction needs to be triggered by button addChocolateChip-btn
+// upon pressing the button it should add a chocolate chip to cookies selected from the dropdown
+function addChocolateChip() {
+
+  updateDisplay();
+}
+
+function updateDisplay() {
+
+  drawCookies();
+}
--- models/Cookie.ts
+++ models/Cookie.ts
@@ -1,4 +1,6 @@
 export class Cookie {
-  //TODO
+  //TODO - Cookies have three attributes; name, colour, chocolateChipNum
+  // colour is a string
+  // There is one constructor which takes a name as a parameter.  Initialise colour to brown and chocolateChipNum to 0
 
 }
